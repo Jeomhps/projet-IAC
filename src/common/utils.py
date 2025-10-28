c9c0b938b1406dd727e6b172794ab04bae366d5c
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2025-10-28 23:27:41 +0100

    refactor : remove all pythons common files

--- src/common/utils.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def sha512_hash_openssl(password: str) -> str:
-    result = subprocess.run(['openssl', 'passwd', '-6', password], capture_output=True, text=True)
-    return result.stdout.strip()
